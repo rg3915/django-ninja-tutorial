147ec4e879e3297caddd57b530f412b61b1cce33
Regis Santos <regis42santos@gmail.com>
2021-06-04 13:35:00 -0300

    edit models

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField('título', max_length=100)
+    description = models.TextField('descrição', null=True, blank=True)
+    due_date = models.DateField('data', null=True, blank=True)
+    is_done = models.BooleanField('pronto', default=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='usuário',
+        related_name='todos',
+    )
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'todo'
+        verbose_name_plural = 'todos'
+
+    def __str__(self):
+        return self.title
