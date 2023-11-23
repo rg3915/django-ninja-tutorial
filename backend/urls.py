76a7066419cb43b1dffc0d4a088dc1d508f903b5
rg3915 <regis42santos@gmail.com>
2023-11-23 19:55:43 -0300

    Adiciona index.

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from .api import api
 
 urlpatterns = [
+    path('', include('backend.core.urls', namespace='core')),
     path('admin/', admin.site.urls),
     path('api/v1/', api.urls),
 ]
