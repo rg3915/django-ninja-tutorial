2c646bb1f6b6f8a921db12076148daa9e7ac80f1
rg3915 <regis42santos@gmail.com>
2023-12-04 08:02:22 -0300

    Adiciona teste para customers. closes #1 #2

--- backend/crm/api.py
+++ backend/crm/api.py
@@ -14,24 +14,27 @@ CustomerSchema = create_schema(Customer)
 
 class CustomerSchemaIn(ModelSchema):
 
-    class Config:
+    class Meta:
         model = Customer
-        model_fields = (
+        fields = (
             'first_name',
             'last_name',
+            'created_by',
         )
 
 
-@router.get("customers/", response=list[CustomerSchema])
+@router.get('/customers', response=list[CustomerSchema])
 def list_customers(request):
     return Customer.objects.all()
 
 
-@router.post('customers/', response={HTTPStatus.CREATED: CustomerSchema})
+@router.post('/customers', response={HTTPStatus.CREATED: CustomerSchema})
 def create_customer(request, payload: CustomerSchemaIn):
-    return Customer.objects.create(**payload.dict())
+    customer = Customer.objects.create(**payload.dict())
+    customer.created_by = request.user
+    return customer
 
 
-@router.get("customers/{id}/", response=CustomerSchema)
+@router.get('/customers/{id}', response=CustomerSchema)
 def get_customer(request, id: int):
     return get_object_or_404(Customer, id=id)
--- backend/crm/tests/test_customer.py
+++ backend/crm/tests/test_customer.py
@@ -1,23 +1,84 @@
+import uuid
+import json
 from http import HTTPStatus
 
 import pytest
 
 
-@pytest.mark.django_db
-def test_list_customer(client):
-    response = client.get('/api/v1/crm/customers/')
-    assert response.status_code == HTTPStatus.OK
-
-
 @pytest.fixture
 def customer_data():
     return {
-        'first_name': 'Regis',
-        'last_name': 'Santos'
+        "first_name": "Regis",
+        "last_name": "Santos",
     }
 
 
+@pytest.fixture
+def test_password():
+    return "strong-test-pass"
+
+
+@pytest.fixture
+def create_user(db, django_user_model, test_password):
+    def make_user(**kwargs):
+        kwargs["password"] = test_password
+        if "username" not in kwargs:
+            kwargs["username"] = str(uuid.uuid4())
+        return django_user_model.objects.create_user(**kwargs)
+
+    return make_user
+
+
+@pytest.fixture
+def auto_login_user(db, client, create_user, test_password):
+    def make_auto_login(user=None):
+        if user is None:
+            user = create_user()
+        client.login(username=user.username, password=test_password)
+        return client
+
+    return make_auto_login
+
+
 @pytest.mark.django_db
-def test_create_customer(client, customer_data):
-    response = client.post('/api/v1/crm/customers/', customer_data)
+def test_list_customer(client):
+    response = client.get("/api/v1/crm/customers/")
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+def test_create_customer(auto_login_user, customer_data):
+    client = auto_login_user()
+    response = client.post(
+        "/api/v1/crm/customers/", customer_data, content_type="application/json"
+    )
+
+    expected = {
+        "id": 1,
+        "first_name": "Regis",
+        "last_name": "Santos",
+        "created_by": 1,
+    }
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert expected == json.loads(response.content)
+
+
+@pytest.mark.django_db
+def test_create_customer_created_by_id_3(auto_login_user, customer_data):
+    _ = auto_login_user()
+    _ = auto_login_user()
+    client = auto_login_user()
+    response = client.post(
+        "/api/v1/crm/customers/", customer_data, content_type="application/json"
+    )
+
+    expected = {
+        "id": 1,
+        "first_name": "Regis",
+        "last_name": "Santos",
+        "created_by": 3,
+    }
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert expected == json.loads(response.content)
